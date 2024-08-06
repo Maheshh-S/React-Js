e12de2f8c0014df187fbae551c0a07d3a9f6ce76
Mahesh <129883017+Maheshh-S@users.noreply.github.com>
2024-08-06 11:17:23 +0000

    02_counter-Done!

--- 02_counter/src/App.jsx
+++ 02_counter/src/App.jsx
@@ -1,40 +1,32 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-import './App.css'
+import "./App.css";
 
 function App() {
-const [counter, setCounter] = useState(0)
+  const [counter, setCounter] = useState(0);
   // let counter = 15
-  const plus = ()=>{
-    console.log("value added",counter);
+  const plus = () => {
+    console.log("value added", counter);
     // counter = counter +1
-    setCounter(counter+1)
-
-   
+    setCounter(counter + 1);
+  };
+  const min = () => {
+    // console.log("value added",counter);
+    // counter = counter +1
+    if (counter > 0) {
+      setCounter(counter - 1);
     }
-    const min = ()=>{
-      // console.log("value added",counter);
-      // counter = counter +1
-      if (counter>0){
-        setCounter(counter-1)
-
-      }
-
-  }
+  };
 
   return (
     <>
-    <h1>chai aur reacttt</h1>
-    <h1>counter value : {counter} </h1>
-<p>Footer :{counter}</p>
-    <button
-    onClick={plus}
-    >plus</button>
-    <button onClick={min}>minusss</button>
-
-
+      <h1>chai aur reacttt</h1>
+      <h1>counter value : {counter} </h1>
+      <p>Footer :{counter}</p>
+      <button onClick={plus}>plus</button>
+      <button onClick={min}>minusss</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
