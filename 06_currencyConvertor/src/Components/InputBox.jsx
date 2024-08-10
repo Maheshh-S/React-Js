be2f55e9cb8301d985d60a0370f80fc66060990f
Mahesh <129883017+Maheshh-S@users.noreply.github.com>
2024-08-10 14:40:34 +0000

    06_currencyConvertor-Done!

--- 06_currencyConvertor/src/Components/InputBox.jsx
+++ 06_currencyConvertor/src/Components/InputBox.jsx
@@ -11,11 +11,11 @@ function InputBox({
     amountDisable = false,
     currencyDisable = false,
     className = "",
-}) {
+}) { 
    const amountInputId = useId()
-
+ 
     return (
-        <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
+         <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
             <div className="w-1/2">
                 <label htmlFor={amountInputId}  className="text-black/40 mb-2 inline-block">
                     {label}
@@ -51,4 +51,4 @@ function InputBox({
     );
 }
 
-export default InputBox;
+export default InputBox; 
